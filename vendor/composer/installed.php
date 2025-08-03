c641cc8b44f7c8f5879396d64a93969e38a4ca6b
sevenjune <yahiawissal76@gmail.com>
2025-08-03 13:14:49 +0100

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v10.3.3',
-        'version' => '10.3.3.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '51ffc6b87b6642219b8b7acde7354df9fe8d9175',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -380,9 +380,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v10.3.3',
-            'version' => '10.3.3.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '51ffc6b87b6642219b8b7acde7354df9fe8d9175',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
