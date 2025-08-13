ce8cc3664fc44db02e1d62f5587624e407306add
sevenjune <yahiawissal76@gmail.com>
2025-08-13 09:16:27 +0100

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,32 +3,36 @@
 @section('title', 'Register')
 
 @section('content')
-<h2>Register</h2>
+<div class="background">
+  <div class="form">
+    <h2>Register</h2>
 
-<form method="POST" action="{{ route('register') }}">
-    @csrf
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
 
-    <label>Username</label>
-    <input type="text" name="username" value="{{ old('username') }}" required>
-    @error('username') <span>{{ $message }}</span> @enderror
+        <label></label>
+        <input type="text" name="username" placeholder="username" value="{{ old('username') }}" required>
+        @error('username') <span>{{ $message }}</span> @enderror
 
-    <label>Email</label>
-    <input type="email" name="email" value="{{ old('email') }}" required>
-    @error('email') <span>{{ $message }}</span> @enderror
+        <label></label>
+        <input type="email" name="email" placeholder="email" value="{{ old('email') }}" required>
+        @error('email') <span>{{ $message }}</span> @enderror
 
-    <label>Phone</label>
-    <input type="text" name="phone" value="{{ old('phone') }}" required>
-    @error('phone') <span>{{ $message }}</span> @enderror
+        <label></label>
+        <input type="text" name="phone" placeholder="phone" value="{{ old('phone') }}" required>
+        @error('phone') <span>{{ $message }}</span> @enderror
 
-    <label>Password</label>
-    <input type="password" name="password" required>
-    @error('password') <span>{{ $message }}</span> @enderror
+        <label></label>
+        <input type="password" name="password" placeholder="password" required>
+        @error('password') <span>{{ $message }}</span> @enderror
 
-    <label>Confirm Password</label>
-    <input type="password" name="password_confirmation" required>
+        <label></label>
+        <input type="password" name="password_confirmation" placeholder="confirm password" required>
+         <br>
+        <button type="submit">Register</button>
+    </form>
 
-    <button type="submit">Register</button>
-</form>
-
-<p>Already have an account? <a href="{{ route('login') }}">Login</a></p>
+    <p>Already have an account? <a href="{{ route('login') }}">Login</a></p>
+  </div>
+</div>
 @endsection
