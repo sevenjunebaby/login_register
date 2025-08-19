e38d381536394838f11ad33e711e28bb11b26ffe
sevenjune <yahiawissal76@gmail.com>
2025-08-19 12:15:35 +0100

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+@section('title', 'Welcome')
 
-                <div class="card-body">
+@section('content') 
+
+
+<div class="background">
+
+
+
+<form class="form" method="POST" action="{{ route('logout') }}">
+    <h1>Welcome, {{ auth()->user()->name }}!</h1>
+    <div {{ __('Dashboard') }}>
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -16,8 +20,9 @@
 
                     {{ __('You are logged in!') }}
                 </div>
-            </div>
-        </div>
-    </div>
+    @csrf
+    <button type="submit">Logout</button>
+</form>
+
 </div>
 @endsection
